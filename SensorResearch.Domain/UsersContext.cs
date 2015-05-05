fa0063c6313e353bd79bac49c0aef4c67a59043d
Ivan Milyutkin <ivan.milyutkin@gmail.com>
2015-05-05 22:09:48 +0300

    moved in different projects models

---
+++ SensorResearch.Domain/UsersContext.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Data.Entity;
+using SensorResearch.Domain.Models;
+
+namespace SensorResearch.Domain
+{
+    public class UsersContext : DbContext, IDisposable
+    {
+        public UsersContext()
+            : base("DefaultConnection")
+        {
+        }
+
+        public DbSet<UserProfile> UserProfiles { get; set; }
+    }
+}
--- SensorResearch.Web/Models/AccountModels.cs
+++ SensorResearch.Web/Models/AccountModels.cs
@@ -1,32 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
-using System.ComponentModel.DataAnnotations.Schema;
-using System.Data.Entity;
 using System.Globalization;
 using System.Web.Security;
 
 namespace SensorResearch.Web.Models
 {
-    public class UsersContext : DbContext
-    {
-        public UsersContext()
-            : base("DefaultConnection")
-        {
-        }
-
-        public DbSet<UserProfile> UserProfiles { get; set; }
-    }
-
-    [Table("UserProfile")]
-    public class UserProfile
-    {
-        [Key]
-        [DatabaseGeneratedAttribute(DatabaseGeneratedOption.Identity)]
-        public int UserId { get; set; }
-        public string UserName { get; set; }
-    }
-
     public class RegisterExternalLoginModel
     {
         [Required]
